8be1f386825283a13abcdb3cb19070d02c80162c
LIYIBEME Hussein <liyibemehussein@gmail.com>
2025-01-29 16:03:53 +0100

    generateur de musique algorithmique.cpp

--- projet gap.cpp
+++ projet gap.cpp
@@ -1,40 +1,44 @@
+//biblioth√®que pour les entr√©es et sorties des variables
 #include <iostream>
+//biblioth√®que pour l'utilisation des tableaux dynamiques
 #include <vector>
+//biblioth√®que pour l'utilisation des fonctions  
 #include <cstdlib>
+//biblioth√®que contient des d√©finitions de fonctions permettant d'obtenir et de manipuler des informations de date et d'heure.
 #include <ctime>
 
-//fonction pour la generation des notes alÈatoires
+//fonction pour la generation des notes al√©atoires
 std::string notealeatoire(){
     //liste des notes de base
 		std::vector<std::string>notes= {"A","B","C","D","E","F","G"};
 
-	//ajout des bemols et diËses,l'ajout du vide ("") pour plus de combinaisons
+	//ajout des bemols et di√®ses,l'ajout du vide ("") pour plus de combinaisons
         std::vector<std::string> notessupplementaires= {"","#","b",};
 
-    //selection alÈatoire d'une note de base
+    //selection al√©atoire d'une note de base
         std::string note = notes[rand() % notes.size()];
 
-    //selection alÈatoire des notes supplÈmentaires
+    //selection al√©atoire des notes suppl√©mentaires
         std::string notessupplementaire = notessupplementaires[rand() % notessupplementaires.size()];
 
-return note +   notessupplementaire;
+return note +   notessupplementaire ;
 }
 
 //Fonction principale
     int main (){
 
-//initialisation de la seclection alÈatoire
+//initialisation de la selection al√©atoire si cette instuction disparait alors il n'y aurait plus de generation aleatoire pour le prochain lancement
     std::srand(std::time(0));
 
 //interaction avec l'utilisateur
-        int nombreDeNotes;
+        int nombrenotes;
         std::cout << "Combien de notes voulez-vous generer ? ";
-        std::cin >> nombreDeNotes;
+        std::cin >> nombrenotes;
 
-//appel de la fontion et affichage du rÈsultat
+//appel de la fontion et affichage du r√©sultat
         std::cout << "\n Sequence de notes aleatoires :\n";
     for (int i = 0; i < nombreDeNotes; ++i) {
-        std::cout << notealeatoire() << " ";
+        std::cout << notealeatoire() << " " ;
     }
 
         std::cout << "\n";
